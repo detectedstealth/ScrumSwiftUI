dd5e691374b671c42d84d24db47a8f8b7203c40b
Bruce Wade <bruce.wade@gmail.com>
2024-02-02 00:57:23 -0800

    Initial project

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Bruce Wade on 2024-02-02.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
