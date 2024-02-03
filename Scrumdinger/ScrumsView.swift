1794db61647726107c0ad4e068000ca01044649e
Bruce Wade <bruce.wade@gmail.com>
2024-02-02 22:36:50 -0800

    Added list view to display all scrums
    Setup Scrum View to be the root view of the app.

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,23 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Bruce Wade on 2024-02-02.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
